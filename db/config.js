47303c0d7da16a60bef57d2d0b46c0734fa86c65
Rabindranath Ferreira <ferreiralox@gmail.com>
2022-09-15 17:38:53 +0200

    add headers into vercel.json for CORS Error on react

--- db/config.js
+++ db/config.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const dbConnection = async() => {
-  try {    await mongoose.connect(process.env.DB_CNN);
+  try {
+    await mongoose.connect(process.env.DB_CNN);
     console.log('DB connected***');
   } catch (error) {
     console.log(error);
